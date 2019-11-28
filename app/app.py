140348b81d34a21eae39a3f0896f55eff82074bc
Hiram Foster <hfoster@prevedere.com>
2019-11-28 15:53:25 -0500

    initial commit

---
+++ app/app.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import os
+
+DEBUG = int(os.environ.get('DEBUG', default=0))
+RELOAD = int(os.environ.get('RELOAD', default=1))
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+app.layout = html.Div(children=[
+    html.H1(children='Hello Dash'),
+
+    html.Div(children='''
+        Dash: A web application framework for Python.
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure={
+            'data': [
+                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
+                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
+            ],
+            'layout': {
+                'title': 'Dash Data Visualization'
+            }
+        }
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(host="0.0.0.0", debug=DEBUG, use_reloader=RELOAD)
